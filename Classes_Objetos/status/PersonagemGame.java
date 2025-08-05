e7eac256493c1bbd295256a77e7fb95ac3a2aa97
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-08-05 19:59:53 -0300

    Status do Personagem

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,52 @@
+public class PersonagemGame {
+	private int saudeAtual;
+	private String nome;
+	private String status;
+
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+		if (saudeAtual > 0) {
+			status = "vivo";
+		}
+
+		if (saudeAtual == 0) {
+			status = "morto";
+		}
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public void tomarDano(int quantidadeDeDano) {
+		setSaudeAtual(saudeAtual - quantidadeDeDano);
+		if (saudeAtual < 0) {
+			setSaudeAtual(0);
+		}
+
+	}
+
+	public void receberCura(int quantidadeDeCura) {
+		setSaudeAtual(saudeAtual + quantidadeDeCura);
+		if (saudeAtual > 100) {
+			setSaudeAtual(100);
+		}
+
+	}
+}

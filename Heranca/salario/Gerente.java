ad393c3bc0863a7239d6560a28c962e66f23c2c2
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-08-06 16:42:35 -0300

    Ajuste na regra salário gerente

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -7,7 +7,8 @@ public class Gerente extends Empregado {
 	@Override
 	public double calcularBonus(Departamento departamento) {
 		if (departamento.getValorAtingidoMeta() >= departamento.getValorMeta()) {
-			return (getSalarioFixo() * 0.2);
+			double diferenca = departamento.getValorAtingidoMeta() - departamento.getValorMeta();
+			return (getSalarioFixo() * 0.2) + (diferenca * 0.01);
 		}
 		return 0;
 	}

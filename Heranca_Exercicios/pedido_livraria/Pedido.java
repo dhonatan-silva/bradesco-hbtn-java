44deb7323701ee37dc4509c9a45ed297aeef84fb
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-08-07 18:20:35 -0300

    Exercício Herança

---
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,39 @@
+public class Pedido {
+	private double percentualDesconto;
+	private ItemPedido[] itens;
+
+	public Pedido(int percentualDesconto, ItemPedido[] itens) {
+		this.percentualDesconto = percentualDesconto;
+		this.itens = itens;
+	}
+
+	public double calcularTotal() {
+		double total = 0;
+		for (ItemPedido itemPedido : itens) {
+			total = total + itemPedido.getQuantidade() * itemPedido.getProduto().obterPrecoLiquido();
+		}
+
+		if (percentualDesconto > 0) {
+			double desconto = total * (percentualDesconto / 100);
+			total = total - desconto;
+		}
+		return total;
+	}
+
+	public double getPercentualDesconto() {
+		return percentualDesconto;
+	}
+
+	public void setPercentualDesconto(double percentualDesconto) {
+		this.percentualDesconto = percentualDesconto;
+	}
+
+	public ItemPedido[] getItens() {
+		return itens;
+	}
+
+	public void setItens(ItemPedido[] itens) {
+		this.itens = itens;
+	}
+
+}

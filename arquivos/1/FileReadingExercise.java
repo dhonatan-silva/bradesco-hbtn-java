a1cfbc3f9f7e0cfb4b54e7d89155bafa985f0343
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-08-13 08:51:26 -0300

    Atividade: Leitura de arquivos

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingExercise {
+
+	public static void main(String[] args) {
+		String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+
+		// efetue a leitura do arquivo
+
+		File arquivo = new File("C:\\Users\\dhona\\OneDrive\\Documentos\\teste\\" + fileName);
+
+		try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+			String linha;
+			while ((linha = br.readLine()) != null) {
+				System.out.println(linha);
+			}
+		} catch (IOException e) {
+			System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+		}
+
+	}
+
+}

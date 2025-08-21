24a007f3f033f08317122fdf2457b6ed2e01ad20
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-08-21 15:23:54 -0300

    Cookies

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+	private List<PedidoCookie> cookies;
+
+	public Pedido() {
+		cookies = new ArrayList<>();
+	}
+
+	public void adicionarPedidoCookie(PedidoCookie pedidoCookie) {
+		cookies.add(pedidoCookie);
+	}
+
+	public int obterTotalCaixas() {
+		int total = 0;
+		for (PedidoCookie pedidoCookie : cookies) {
+			total += pedidoCookie.getQuantidadeCaixas();
+		}
+		return total;
+	}
+
+	public int removerSabor(String sabor) {
+		int total = 0;
+		for (int i = 0; i < cookies.size(); i++) {
+			PedidoCookie pedidoCookie = cookies.get(i);
+			if (pedidoCookie.getSabor().equals(sabor)) {
+				cookies.remove(i);
+				total += pedidoCookie.getQuantidadeCaixas();
+			}
+		}
+		return total;
+	}
+
+}

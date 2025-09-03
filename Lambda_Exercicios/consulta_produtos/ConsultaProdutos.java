23106feb88606403df5928b9acd4556f800acacf
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-09-03 19:46:34 -0300

    Consultar Produtos

--- Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 public class ConsultaProdutos {
 
-	public static ArrayList<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro) {
+	public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterioFiltro) {
 		return (ArrayList<Produto>) produtos.stream()
 											 .filter(x -> criterioFiltro.testar(x))
 											 .collect(Collectors.toList());

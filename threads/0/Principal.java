0eebf1d599e2a6e2ef162cff00cfe8ee7815d3f7
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-09-20 11:45:08 -0300

    Sincronização com Threads

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -1,13 +1,17 @@
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Contador contador = new Contador();
 
-		Thread t1 = new Thread(new ThreadContador(contador));
-		Thread t2 = new Thread(new ThreadContador(contador));
+		ThreadContador tc = new ThreadContador(contador);
 
-		t1.run();
-		t2.run();
+		Thread t1 = new Thread(tc);
+		Thread t2 = new Thread(tc);
+
+		t1.start();
+		t1.join();
+		t2.start();
+		t2.join();
 
 		System.out.println("Valor final do contador: " + contador.getContagem());
 	}

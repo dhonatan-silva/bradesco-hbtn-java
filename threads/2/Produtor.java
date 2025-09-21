0bd692fbb413d105d598fc25de0e44f41071504c
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-09-21 14:27:01 -0300

    Simulação de um Sistema de Filas com Produtores e Consumidores

--- threads/2/Produtor.java
+++ threads/2/Produtor.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Produtor implements Runnable {
 	private Fila fila;
 
@@ -9,7 +10,9 @@ public class Produtor implements Runnable {
 	public void run() {
 		try {
 			for (int i = 1; i <= fila.getCapacidade(); i++) {
-				fila.adicionar(i); // O produtor coloca os itens na lista
+				Random random = new Random();
+				int numero = random.nextInt(100) + 1;
+				fila.adicionar(numero); // O produtor coloca os itens na lista
 				Thread.sleep(500); // Simula o tempo para produzir um item
 			}
 		} catch (InterruptedException e) {
